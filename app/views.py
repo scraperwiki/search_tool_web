bbcf45f326b9532cd20572901fb3f22513857db5
Steven Maude <steve@github.ubuntu>
2015-05-19 11:34:44 +0100

    Placeholder search route

--- app/views.py
+++ app/views.py
@@ -2,9 +2,17 @@
 # encoding: utf-8
 from __future__ import (unicode_literals, division,
                         print_function, absolute_import)
+
+from flask import request
 from app import app
 
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+
+@app.route('/search')
+def search():
+    # query = request.args.get('q')
+    return '', 204
